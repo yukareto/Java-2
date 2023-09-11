042985204422a866b76dc85f22e18faab470ef35
yureto <yut465p@gamil.com>
2023-09-11 15:59:31 +0900

    第2回課題

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String name;
+        Cat cat = new Cat("Tama");
+        System.out.println("Hello," + cat.getName() + "!");
+    }
+}
